b9f9af3f647c49bbaaa3b96666aec1e756df4eff
ziko278 <odekeziko@gmail.com>
2025-12-08 15:44:53 +0100

    new ups

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = 'http://localhost:8000/api' //'https://api.mattressmarket.ng/api';
+export const API_BASE_URL = 'https://api.mattressmarket.ng/api'; // http://localhost:8000/api'
 
 // Color Scheme
 export const COLORS = {
